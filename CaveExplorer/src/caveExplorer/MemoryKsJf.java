6841839630e0888bb1c66c102cbd0b140b2e1b64
Student 8 <Student 8@10.8.33.163>
2016-11-10 14:00:26 -0500

    begin work

---
+++ CaveExplorer/src/caveExplorer/MemoryKsJf.java
@@ -0,0 +1,10 @@
+package caveExplorer;
+
+public class MemoryKsJf extends CaveRoomPd8 {
+
+	public MemoryKsJf(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
