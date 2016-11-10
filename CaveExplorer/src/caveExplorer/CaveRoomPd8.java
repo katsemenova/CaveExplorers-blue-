5ec79f1af7289a94f612d6a78fc5101e4b483a6b
Student 8 <Student 8@10.8.33.163>
2016-11-10 13:28:44 -0500

    helper method

--- CaveExplorer/src/caveExplorer/CaveRoomPd8.java
+++ CaveExplorer/src/caveExplorer/CaveRoomPd8.java
@@ -117,13 +117,17 @@ public class CaveRoomPd8 {
 				break;
 			}
 		}
+		goToRoom(indexFound);
+		
+	}
+
+	private void goToRoom(int indexFound) {
 		if(borderingRooms[indexFound]!= null&&doors[indexFound]!=null&&doors[indexFound].isOpen()){
 			CaveExplorer.currentRoom.leave();
 			CaveExplorer.currentRoom=borderingRooms[indexFound];
 			CaveExplorer.currentRoom.enter();
 			CaveExplorer.inventory.updateMap();
 		}
-		
 	}
 
 	private boolean isValid(String input) {
