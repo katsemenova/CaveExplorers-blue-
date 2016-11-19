91f2f13c779f61d17f2ba611485d28a19a985206
Jenniber Franco <jfranco7156@bths.edu>
2016-11-19 14:01:28 -0500

    Complete endEvent

--- CaveExplorer/src/caveExplorer/GameEndEvent.java
+++ CaveExplorer/src/caveExplorer/GameEndEvent.java
@@ -2,12 +2,13 @@ package caveExplorer;
 
 public class GameEndEvent implements Playable {
 
-	private static final String[] SEQUENCE={"<You slowly exit the castle>"};
+	private static final String[] SEQUENCE={"<You slowly exit the castle>","<Entering a forest with thick fog, you follow the only path you can see in front of you>","<While walking, you realize that you are slowly feeling more and more drowsy>","<You feel the darkness enveloping you>", "<Suddenly, you find yourself waking up, having left Wonderland and your adventure through the castle behind>"};
 	public GameEndEvent() {
 	}
 
 	public void play(){
 		readSequence(SEQUENCE);
+		System.out.println("The End");
 	}
 	
 	public static void readSequence(String[] seq){
